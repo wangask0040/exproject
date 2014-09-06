bb219d0f8dd5631ae9a6b75d9c73151df371a456
patrickwang <63165862@qq.com>
2014-09-06 08:28:01 +0800

    a

--- getway/main.cpp
+++ getway/main.cpp
@@ -8,10 +8,11 @@ using namespace std;
 #include "RegisterReq.h"
 
 
-void AcceptCB(struct evconnlistener * p, evutil_socket_t fd, struct sockaddr * addr, int socklen, void * data);
-void ReadCB(struct bufferevent *bev, void *ctx);
-void WriteCB(struct bufferevent *bev, void *ctx);
-void EventCB(struct bufferevent *bev, short what, void *ctx);
+void OnAccept(struct evconnlistener * p, evutil_socket_t fd, struct sockaddr * addr, int socklen, void * data);
+void OnRead(struct bufferevent *bev, void *ctx);
+void OnWrite(struct bufferevent *bev, void *ctx);
+void OnEvent(struct bufferevent *bev, short what, void *ctx);
+void OnTimer(evutil_socket_t fd, short what, void* ctx);
 
 int main(int argc, char** argv)
 {
@@ -25,16 +26,23 @@ int main(int argc, char** argv)
 
 
 	CRegisterReq rr;
-
 	event_base* base = event_base_new();
 	sockaddr addr;
 	int len = sizeof(addr);
 	evutil_parse_sockaddr_port("127.0.0.1:5000", &addr, &len);
 	evconnlistener*  listener =
-		evconnlistener_new_bind(base, AcceptCB, &rr, LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, 10, &addr, len);
+		evconnlistener_new_bind(base, OnAccept, &rr, LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, 10, &addr, len);
+
+	event* evtime = nullptr;
+	evtime = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, OnTimer, &evtime);
+	timeval tm;
+	tm.tv_sec = 1;
+	tm.tv_usec = 1;
+	evtimer_add(evtime, &tm);
 
 	event_base_dispatch(base);
 
+	evtimer_del(evtime);
 	evconnlistener_free(listener);
 	event_base_free(base);
 	
@@ -45,20 +53,20 @@ int main(int argc, char** argv)
 	return 1;
 }
 
-void AcceptCB(struct evconnlistener * p, evutil_socket_t fd, struct sockaddr * addr, int socklen, void * data)
+void OnAccept(struct evconnlistener * p, evutil_socket_t fd, struct sockaddr * addr, int socklen, void * data)
 {
 	addr;
 	socklen;
 	event_base* base = evconnlistener_get_base(p);
 	bufferevent* bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
-	bufferevent_setcb(bev, ReadCB, WriteCB, EventCB, data);
+	bufferevent_setcb(bev, OnRead, OnWrite, OnEvent, data);
 	bufferevent_enable(bev, EV_WRITE | EV_READ);
 	char bufaddr[128] = { 0 };
 	evutil_inet_ntop(addr->sa_family, addr, bufaddr, sizeof(bufaddr));
 	cout << "accept connection socket id = " << fd << "ipaddress =" << bufaddr << endl;
 }
 
-void ReadCB(struct bufferevent *bev, void *ctx)
+void OnRead(struct bufferevent *bev, void *ctx)
 {
 	CRegisterReq* q = reinterpret_cast<CRegisterReq*>(ctx);
 
@@ -78,16 +86,24 @@ void ReadCB(struct bufferevent *bev, void *ctx)
 	ret = q->RecvData(evoutput);
 }
 
-void WriteCB(struct bufferevent *bev, void *ctx)
+void OnWrite(struct bufferevent *bev, void *ctx)
 {
 	bev;
 	ctx;
 	cout << "into WriteCB" << endl;
 }
 
-void EventCB(struct bufferevent *bev, short what, void *ctx)
+void OnEvent(struct bufferevent *bev, short what, void *ctx)
 {
 	bev;
 	ctx;
 	cout << "into EventCB what = " << what << endl;
+}
+
+void OnTimer(evutil_socket_t fd, short what, void* ctx)
+{
+	fd; 
+	what;
+	ctx;
+	cout << "into Ontimer what=" << what << endl;
 }
